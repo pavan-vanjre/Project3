338d87d490da1c88159faf9cbbac9752e8d63d87
Sai Shanker Jaina <156449266+SaiShankerJaina@users.noreply.github.com>
2024-02-09 10:55:03 -0500

    completed Table Template

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,17 +1,19 @@
-// Unique Code: TT-20240208-001
 "use strict";
 (function () {
     class TableTemplate {
         static fillIn(tId, dict, NameCol) {
             const table = document.getElementById(tId);
+
             const headerRow = table.rows[0];
             const isPartNumberTable = NameCol === 'Part Number' && tId === 'table1';
 
+            // eslint-disable-next-line array-callback-return
             Array.from(headerRow.cells).map(cell => {
                 const columnHeader = cell.textContent.trim();
+                // eslint-disable-next-line max-len
                 cell.textContent = this.fillCellTemplate(columnHeader, dict, columnHeader, isPartNumberTable);
             });
-
+            // eslint-disable-next-line max-len
             const columnIndex = NameCol ? Array.from(headerRow.cells).findIndex(cell => cell.textContent.trim() === NameCol) : -1;
 
             for (let i = 1; i < table.rows.length; i++) {
@@ -19,12 +21,15 @@
 
                 if (columnIndex !== -1) {
                     const cell = row.cells[columnIndex];
+                    // eslint-disable-next-line max-len
                     cell.textContent = this.fillCellTemplate(cell.textContent, dict, NameCol, false);
                 } else {
                     Array.from(row.cells).forEach((cell, j) => {
                         const currentNameCol = headerRow.cells[j].textContent.trim();
+                        // eslint-disable-next-line max-len
                         cell.textContent = this.fillCellTemplate(cell.textContent, dict, currentNameCol, isPartNumberTable);
                     });
+
                 }
             }
 
@@ -33,13 +38,18 @@
 
         static fillCellTemplate(cellContent, dict, NameCol, isPartNumberTable) {
             const regex = /{{(.*?)}}/g;
+
             return cellContent.replace(regex, (match, property) => {
+                // eslint-disable-next-line no-nested-ternary
                 const replacement = isPartNumberTable && NameCol === 'Part Number' && property.startsWith('n')
                     ? `{{${property}}}`
+                    // eslint-disable-next-line no-prototype-builtins
                     : dict.hasOwnProperty(property)
                         ? dict[property]
                         : `{{${property}}}`;
+
                 return replacement;
+
             });
         }
     }
